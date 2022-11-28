35dff2e732a9a09e06465b2cae1e398776f1aeef
Darya Shnipova <dshnipova@gmail.com>
2022-11-28 09:31:58 +0100

    Added refresh plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,9 +2,10 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-	return  [
+	const plugins = [
 		new HtmlWebpackPlugin({ template: paths.html }),
 		new webpack.ProgressPlugin(),
 		new MiniCssExtractPlugin({
@@ -13,6 +14,13 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
 		}),
 		new webpack.DefinePlugin({
 			__IS_DEV__: JSON.stringify(isDev)
-		})
-	]
+		}),
+		new webpack.HotModuleReplacementPlugin()
+	];
+	
+	if (isDev) {
+		plugins.push(new ReactRefreshWebpackPlugin())
+	}
+	
+	return plugins;
 }
