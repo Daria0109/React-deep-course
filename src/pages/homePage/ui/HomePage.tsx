83b992eaa498edf21d0d153927974cfaad1883a3
Darya Shnipova <dshnipova@gmail.com>
2022-12-21 23:25:29 +0100

    Removed console

--- src/pages/homePage/ui/HomePage.tsx
+++ src/pages/homePage/ui/HomePage.tsx
@@ -3,8 +3,6 @@ import { useTranslation } from 'react-i18next';
 const HomePage = (): JSX.Element => {
 	const { t } = useTranslation('home');
 
-	console.log();
-
 	return (
 		<div>
 			{t('home')}
