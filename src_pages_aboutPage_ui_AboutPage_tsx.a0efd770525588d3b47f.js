7e78dc74faeccf69508899e4617b93a8e9fd80d7
Darya Shnipova <dshnipova@gmail.com>
2022-12-24 23:38:10 +0100

    Updates

---
+++ src_pages_aboutPage_ui_AboutPage_tsx.a0efd770525588d3b47f.js
@@ -0,0 +1,46 @@
+"use strict";
+(self["webpackChunkreact_deep_course"] = self["webpackChunkreact_deep_course"] || []).push([["src_pages_aboutPage_ui_AboutPage_tsx"],{
+
+/***/ "./src/pages/aboutPage/ui/AboutPage.tsx":
+/*!**********************************************!*\
+  !*** ./src/pages/aboutPage/ui/AboutPage.tsx ***!
+  \**********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+/* harmony import */ var react_i18next__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-i18next */ "./node_modules/react-i18next/dist/es/useTranslation.js");
+/* provided dependency */ var __react_refresh_utils__ = __webpack_require__(/*! ./node_modules/@pmmmwh/react-refresh-webpack-plugin/lib/runtime/RefreshUtils.js */ "./node_modules/@pmmmwh/react-refresh-webpack-plugin/lib/runtime/RefreshUtils.js");
+__webpack_require__.$Refresh$.runtime = __webpack_require__(/*! ./node_modules/react-refresh/runtime.js */ "./node_modules/react-refresh/runtime.js");
+
+
+
+var AboutPage = function () {
+    var t = (0,react_i18next__WEBPACK_IMPORTED_MODULE_1__.useTranslation)('about').t;
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: t('about') }));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AboutPage);
+
+
+const $ReactRefreshModuleId$ = __webpack_require__.$Refresh$.moduleId;
+const $ReactRefreshCurrentExports$ = __react_refresh_utils__.getModuleExports(
+	$ReactRefreshModuleId$
+);
+
+function $ReactRefreshModuleRuntime$(exports) {
+	if (false) {}
+}
+
+if (typeof Promise !== 'undefined' && $ReactRefreshCurrentExports$ instanceof Promise) {
+	$ReactRefreshCurrentExports$.then($ReactRefreshModuleRuntime$);
+} else {
+	$ReactRefreshModuleRuntime$($ReactRefreshCurrentExports$);
+}
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX2Fib3V0UGFnZV91aV9BYm91dFBhZ2VfdHN4LmEwZWZkNzcwNTI1NTg4ZDNiNDdmLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7OztBQUFnRDtBQUNEO0FBQy9DO0FBQ0EsWUFBWSw2REFBYztBQUMxQixZQUFZLHNEQUFJO0FBQ2hCO0FBQ0EsaUVBQWUsU0FBUyxFQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVhY3QtZGVlcC1jb3Vyc2UvLi9zcmMvcGFnZXMvYWJvdXRQYWdlL3VpL0Fib3V0UGFnZS50c3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsganN4IGFzIF9qc3ggfSBmcm9tIFwicmVhY3QvanN4LXJ1bnRpbWVcIjtcbmltcG9ydCB7IHVzZVRyYW5zbGF0aW9uIH0gZnJvbSAncmVhY3QtaTE4bmV4dCc7XG52YXIgQWJvdXRQYWdlID0gZnVuY3Rpb24gKCkge1xuICAgIHZhciB0ID0gdXNlVHJhbnNsYXRpb24oJ2Fib3V0JykudDtcbiAgICByZXR1cm4gKF9qc3goXCJkaXZcIiwgeyBjaGlsZHJlbjogdCgnYWJvdXQnKSB9KSk7XG59O1xuZXhwb3J0IGRlZmF1bHQgQWJvdXRQYWdlO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
